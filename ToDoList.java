4bbbb0931b1d0a8c6d6e93375497b8e896a2895c
Michael R Zeller <103454513+MRZeller1@users.noreply.github.com>
2024-10-07 19:47:09 -0500

    Update ToDoList.java

--- ToDoList.java
+++ ToDoList.java
@@ -7,15 +7,15 @@ import java.util.*;
  * Please implement the provided methods and add appropriate comments for each method.
  * Analysis: 
  * The ToDoList class needs to be able take the Task Object we created and make it where we can sort the task in the list,
- * addTask, mark them as complete, print each task, print them by completetion, and search for task. The linked-list is a good
+ * addTask, mark them as complete, print each task, print them by completion, and search for task. The linked-list is a good
  * data structure for this because we dont access any of the task by index so we just use a counter to see how much task we have.
  * With Linked list adding elements is O(1), searching, printing, marking complete, are all O(n) time comlexity which is reasonable
  * but marking task and finding specfic task could be easier in other data structures.
  * Some changes I would make is I'd make a method to delete task. I think another idea would be is using a priority Heap because than
- * the most important task `    acan be easily accesible, but this would likley require us to use more than one data structure for completed
+ * the most important task `can be easily accessible, but this would likely require us to use more than one data structure for completed
  * once a user completes one of the higher priority task it can remove that task than place it into another data structure for completed'
  * task. A min-heap would be best for this so we could place the highest priority as number one and than do the order based of that.
- * This can also be efficent for school task, because we can use method to determine priortiy by due date and classes.
+ * This can also be efficient for school task, because we can use method to determine priortiy by due date and classes.
  */
 class ToDoList {
 	private Node head;
@@ -51,6 +51,7 @@ class ToDoList {
 	}
     
 	/* 
+ 	 * @author Michael
 	 * The add Task method appends a task to the end of the linked list and also increases the Task count
 	 */	
 	public void addTask(Task task) {
@@ -73,15 +74,16 @@ class ToDoList {
         }
     }
 	/*
-     * This method is responsible for displaying all tasks within the list. 
-     * If the list is empty, it will print "No tasks found".
-	 * Note: You must use the provided toString() method of the Task class to format the task details correctly.
-     * 
-     * Write appropriate comment:
-     * The show all task method prints each task, simply by iterating through the linked list, starting from the head
-     * and using the two string method for each task.
-     * 
-	 */
+ 	* @author Michael
+  	* This method is responsible for displaying all tasks within the list. 
+	* If the list is empty, it will print "No tasks found".
+	* Note: You must use the provided toString() method of the Task class to format the task details correctly.
+ 	* 
+	* Write appropriate comment:
+	* The show-all task method prints each task, simply by iterating through the linked list, starting from the head
+	* and using the two-string method for each task.
+	* 
+	*/
 	public void showAllTasks() {
         Node current = this.head;
         if (this.isEmpty()){
@@ -105,7 +107,8 @@ class ToDoList {
      * If a match is not found it will print "Task not found", otherwise "Task marked as completed"
 	 * Note: You must use the overriden equals() method of the Task class to search the task, to get the full credit.
      * 
-     * Write appropriate comment:
+     * @author Michael
+     * Write an appropriate comment:
      *
 	 */
 	public void markTaskAsCompleted(String description) {
@@ -131,9 +134,9 @@ class ToDoList {
 	 * This method filters the tasks based on their completion status. 
      * If a match is not found it will print "No tasks found". 
 	 * Note: You must use the provided toString() method of the Task class to format the task details correctly.
-     * 
-     * Write appropriate comment:
-     * This method starts by checking if the user wants to see the completed task or pending task, than using an if else,
+     * @author Michael
+     * Write an appropriate comment:
+     * This method starts by checking if the user wants to see the completed task or pending task, then using an if-else,
      * iterates through each task and prints each task that is either (complete/pending) depending on what the user wants to
      * see starting from the head of the list.
      */
@@ -174,8 +177,8 @@ class ToDoList {
 	}
     
 	/*
-     * Write appropriate comment:
-     * This method returns the taskCount, which is incrimented with each added element.
+     * Write an appropriate comment:
+     * This method returns the task count, which is incremented with each added element.
 	 */
 	public Integer getTasksCount() {
         //Write your code here
@@ -183,28 +186,30 @@ class ToDoList {
     }
     
     /*
+     * @author Michael
      * Write appropriate comment:
      * This method sorts the task by the users choice of either description or due date
-     * .equalsIgnoreCase() method is used for this because it being case sensitive doesnt 
-     * matter. I used the Merge Sort algorithm, because I wanted to challege myself and 
-     * because Merge Sort is a good algoritm for linked list due to the fact that you dont have
+     * .equalsIgnoreCase() method is used for this because it is case-sensitive and doesn't 
+     * matter. I used the Merge Sort algorithm, because I wanted to challenge myself and 
+     * because Merge Sort is a good algorithm for linked list due to the fact that you don't have
      * to index, other than getting the size of the linked list and the middle. This sortTask method
      * is comprised of 3 different helper methods, first mergeSort takes the start of the linked list
-     * and a boolean value for if it is being sorted by description or not. Than using recurison
+     * and a boolean value for if it is being sorted by description or not. Then using recursion
      * the base case checks if the list passed is either of size one or empty. This is important 
-     * because what the recrusive method is doing is splitting the list into smaller sub list.
-     * Than in the else case, it is finding the middle of the given list by using a helper method get
+     * because what the recursive method is doing is splitting the list into smaller sub-lists.
+     * Then in the else case, it is finding the middle of the given list by using a helper method get
      * middle, which has two runners, a fast runner that goes through the list 2 nodes at a time
-     * and a slow runner that goes one node at the time. This finds the middle because by the time the
+     * and a slow runner that goes one node at the time. This finds the middle because, by the time the
      * fast node reaches the end of the list, the slow node will be at the middle because it is iterating
      * twice as slow. Then the first list is split by setting the right list = to the node right of the middle
-     * and the than we set that node in the original list equal to null efectivly splitting the list into two
-     * left and right sublist. Than we call the mergeSort() method again but this time for the first half(left) 
-     * and second half(right) of the linked list. Than after these are iterated through the merge method is than used
-     * to merge the smalles sublist of size 1, than the next sublist size 2, all the way up until size n when
-     * their is just one list left, our sorted list. I used the overrided compareTo() to sort the descitpion 
+     * and then we set that node in the original list equal to null effectively splitting the list into two
+     * left and right sublist. Then we call the mergeSort() method again but this time for the first half(left) 
+     * and second half(right) of the linked list. Then after these are iterated through the merge method is then used
+     * to merge the small sublist of size 1, then the next sublist size 2, all the way up until size n when
+     * there is just one list left, our sorted list. I used the overridden compareTo() to sort the description 
      * and I made a method that converts the date to an integer to sort by date from soonest date to furthest.
 	 */
+	
 	public void sortTasks(String sortBy) {
         if(sortBy.equalsIgnoreCase("description")){
             head = mergeSort(this.head, true);
